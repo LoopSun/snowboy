557a38867ddd6688266d934a567baaf4eae9fe83
loopsun <zhouhua@richinfo.cn>
2018-10-02 16:19:48 +0800

    Feature: 提交snowboy

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,4 +1,1 @@
 docker build -t snowboy:latest  -f docker/Dockerfile .
-docker tag snowboy:latest registry.cmic.site:5000/sg/snowboy:latest
-docker push registry.cmic.site:5000/sg/snowboy:latest
-docker rmi $(docker images -f "dangling=true" -q)
